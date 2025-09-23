072182bdbac83fabad1d9cbd391b42ee0f056275
Crisdell James Ines <crisdelli@codev.com>
2025-09-23 12:08:40 +0800

    Add Odin-Connect

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { OdinConnect, OdinUser } from 'inescri-connect';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +9,22 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  private odin: OdinConnect;
+  
   title = 'connect-external-demo';
+  user: OdinUser | null = null;
+
+
+  constructor() {
+    this.odin = new OdinConnect({ name: "External Demo", env: "local" });
+  }
+
+  async connect() {
+   this.user = await this.odin.connect({
+      open: {
+        settings: 'height=700,width=500',
+        target: '_blank'
+      }
+    });
+  }
 }
