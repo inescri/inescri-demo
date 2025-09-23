4c3dcd5501a0f0fea6fdaf9a3210d1690164c8f0
Crisdell James Ines <crisdelli@codev.com>
2025-09-23 13:33:07 +0800

    use preview environment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,14 +16,14 @@ export class AppComponent {
 
 
   constructor() {
-    this.odin = new OdinConnect({ name: "External Demo", env: "local" });
+    this.odin = new OdinConnect({ name: "External Demo", env: 'preview' });
   }
 
   async connect() {
    this.user = await this.odin.connect({
       open: {
         settings: 'height=700,width=500',
-        target: '_blank'
+        target: '_blank',
       }
     });
   }
